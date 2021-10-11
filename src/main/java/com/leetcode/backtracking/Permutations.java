11c2778e5786e38c8a53f800626a0f733ff7e5b8
shuangmu <wangchenglinjob@163.com>
2021-10-11 17:22:13 +0800

    排列问题，通过交换解决

--- src/main/java/com/leetcode/backtracking/Permutations.java
+++ src/main/java/com/leetcode/backtracking/Permutations.java
@@ -1,45 +1,43 @@
 package com.leetcode.backtracking;
 
-import java.util.List;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
+ * 实现排列 (不用添加，用交换数据完成)
+ * {@link "https://leetcode.com/problems/permutations/"}
+ *
  * @author zms
  * @date 5:48 PM 2020/2/25
- * {@link "https://leetcode.com/problems/permut ations/"}
- * TODO 实现排列 (不用添加，用交换数据完成)
  */
 public class Permutations {
     /**
      * Given a collection of distinct integers, return all possible permutations.
      * Example:
-     *      Input: [1,2,3]
-     *      Output:
-     *          [
-     *              [1,2,3],
-     *              [1,3,2],
-     *              [2,1,3],
-     *              [2,3,1],
-     *              [3,1,2],
-     *              [3,2,1]
-     *          ]
+     * Input: [1,2,3]
+     * Output:
+     * [
+     * [1,2,3],
+     * [1,3,2],
+     * [2,1,3],
+     * [2,3,1],
+     * [3,1,2],
+     * [3,2,1]
+     * ]
      */
     static List<List<Integer>> res;
-    public static List<List<Integer>> permute(int[] nums) {
+
+    public static List<List<Integer>> permute(int[] arr) {
         res = new ArrayList<>();
-        dfs(nums, 0, null, new ArrayList<>());
+        dfs(arr, 0, null, new ArrayList<>());
         return res;
     }
 
-    public static void dfs(int[] nums, int length, Set<Integer> set, List<Integer> list) {
-        if(length == nums.length) {
+    public static void dfs(int[] arr, int length, Set<Integer> set, List<Integer> list) {
+        if (length == arr.length) {
             res.add(list);
             return;
         }
-        for (int num : nums) {
+        for (int num : arr) {
             if (length == 0) {
                 set = new HashSet<>();
             }
@@ -47,22 +45,57 @@ public class Permutations {
                 set.add(num);
                 List<Integer> newList = new ArrayList<>(list);
                 newList.add(num);
-                dfs(nums, length + 1, set, newList);
+                dfs(arr, length + 1, set, newList);
                 set.remove(num);
             }
         }
+    }
+
+    public static List<List<Integer>> permute01(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        process01(arr, 0, res);
+        return res;
+    }
+
+    /**
+     * @param arr   需要排列的数组
+     * @param index 排列数组的当前索引
+     * @param res   结果集合
+     */
+    private static void process01(int[] arr, int index, List<List<Integer>> res) {
+        if (index >= arr.length) {
+            List<Integer> ans = new ArrayList<>();
+            for (int i : arr) {
+                ans.add(i);
+            }
+            res.add(ans);
+            return;
+        }
+        for (int i = index; i < arr.length; i++) {
+            // index == 0 第一个数可以是index到arr.length-1中的任意一个数
+            // 如果一个数是i，那么之后的数就不能有i，就将i放到index位置，那么后续的遍历就不用从头进行，并且不用判断
+            swap(arr, index, i);
+            process01(arr, index + 1, res);
+            swap(arr, index, i);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String[] args) {
 
 //        System.out.println(permute(new int[]{1}));
 //        System.out.println(permute(new int[]{1,2}));
-//        System.out.println(permute(new int[]{1,2,3}));
+        System.out.println(permute01(new int[]{1, 2, 3}));
 //        System.out.println(permute(new int[]{1,2,3,4}));
 //        System.out.println(permute(new int[]{1,2,3,4,5}));
 //        System.out.println(permute(new int[]{1,2,3,4,5,6}));
 //        System.out.println(permute(new int[]{1,2,3,4,5,6,7}));
 //        System.out.println(permute(new int[]{1,2,3,4,5,6,7,8}));
-        System.out.println(permute(new int[]{1,2,3,4,5,6,7,8,9}));
+        System.out.println(permute(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
     }
 }
--- src/main/java/com/leetcode/backtracking/PermutationsII.java
+++ src/main/java/com/leetcode/backtracking/PermutationsII.java
@@ -6,36 +6,41 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * <a href="https://leetcode.com/problems/permutations-ii/">Permutations II</a>
+ *
  * @author zms
  * @date 2:11 PM 2020/5/11
- * <a href="https://leetcode.com/problems/permutations-ii/">
- *  Permutations II</a>
  * @see Permutations
- * TODO 耗时 优化
  */
 public class PermutationsII {
     /**
      * Given a collection of numbers that might contain duplicates,
      * return all possible unique permutations.
-     *
+     * <p>
      * Example:
-     *
+     * <p>
      * Input: [1,1,2]
      * Output:
      * [
-     *   [1,1,2],
-     *   [1,2,1],
-     *   [2,1,1]
+     * [1,1,2],
+     * [1,2,1],
+     * [2,1,1]
      * ]
+     * <p>
+     * Constraints:
+     * 1 <= nums.length <= 8
+     * -10 <= nums[i] <= 10
      */
     static Set<String> set = new HashSet<>();
     static List<List<Integer>> res = new ArrayList<>();
+
     public static List<List<Integer>> permuteUnique(int[] nums) {
         dfs(nums, 0, new boolean[nums.length], new ArrayList<>());
         return res;
     }
+
     public static void dfs(int[] nums, int depth, boolean[] flag, List<Integer> list) {
-        if(depth == nums.length) {
+        if (depth == nums.length) {
             set.add(list.toString());
             res.add(list);
             return;
@@ -43,9 +48,9 @@ public class PermutationsII {
         int length = nums.length;
         for (int i = 0; i < length; i++) {
             List<Integer> newList = new ArrayList<>(list);
-            if(!flag[i]) {
+            if (!flag[i]) {
                 newList.add(nums[i]);
-                if(!set.contains(newList.toString())) {
+                if (!set.contains(newList.toString())) {
                     flag[i] = true;
                     dfs(nums, depth + 1, flag, newList);
                     flag[i] = false;
@@ -56,7 +61,53 @@ public class PermutationsII {
         }
     }
 
+    public static List<List<Integer>> permuteUnique01(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        process01(arr, 0, res);
+        return res;
+    }
+
+    // 剪枝
+    public static void process01(int[] arr, int index, List<List<Integer>> res) {
+        int length = arr.length;
+        if (index == length) {
+            List<Integer> ans = new ArrayList<>();
+            for (int j : arr) {
+                ans.add(j);
+            }
+            res.add(ans);
+            return;
+        }
+        // 判断当前位置是否使用过某一个数
+//        boolean[] visit = new boolean[21];
+        for (int i = index; i < length; i++) {
+//            if (!visit[arr[i] + 10]) {
+            if (isVisit(arr, index, i)) {
+//                visit[arr[i] + 10] = true;
+                swap(arr, index, i);
+                process01(arr, index + 1, res);
+                swap(arr, index, i);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // 判断当前位置是否使用过某一个数
+    private static boolean isVisit(int[] arr, int cur, int index) {
+        for (int i = cur; i < index; i++) {
+            if (arr[i] == arr[index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(permuteUnique(new int[]{1,1,2}));
+        System.out.println(permuteUnique01(new int[]{1, 1, 2}));
     }
 }
