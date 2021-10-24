127dba4776d7b3115771cae9b6cf0e4d99be0a59
shuangmu <wangchenglinjob@163.com>
2021-10-24 21:43:24 +0800

    通用

--- src/main/java/com/leetcode/dp/fibonacci/DeleteAndEarn.java
+++ src/main/java/com/leetcode/dp/fibonacci/DeleteAndEarn.java
@@ -1,5 +1,9 @@
 package com.leetcode.dp.fibonacci;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * @author zms
  * @date 2:08 PM 2020/4/1
@@ -34,7 +38,7 @@ public class DeleteAndEarn {
      *      The length of nums is at most 20000.
      *      Each element nums[i] is an integer in the range [1, 10000].
      */
-    public static int deleteAndEarn(int[] nums) {
+    public static int deleteAndEarn02(int[] nums) {
         if(nums == null || nums.length == 0) {
             return 0;
         }
@@ -52,8 +56,36 @@ public class DeleteAndEarn {
         return second;
     }
 
+    public static int deleteAndEarn(int[] nums) {
+        // 构造一个不重复的排好序的数组
+        Map<Integer, Integer> map = new TreeMap<>();
+        for(int num : nums) {
+            map.merge(num, 1, Integer::sum);
+        }
+        int n = map.size();
+        nums = new int[n];
+        int[] counts = new int[n];
+        int index = 0;
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            nums[index] = entry.getKey();
+            counts[index++] = entry.getValue();
+        }
+        int[][] dp = new int[n + 1][2];
+        dp[1][0] = nums[0] * counts[0];
+        for(int i = 1; i < n; i++) {
+            dp[i + 1][0] = dp[i][1] + nums[i] * counts[i];
+            if(nums[i] - nums[i - 1] != 1) {
+                dp[i + 1][0] = Math.max(dp[i][0] + nums[i] * counts[i], dp[i + 1][0]);
+            }
+            dp[i + 1][1] = Math.max(dp[i][0],dp[i][1]);
+        }
+
+        return Math.max(dp[n][0], dp[n][1]);
+    }
+
     public static void main(String[] args) {
-        System.out.println(deleteAndEarn(new int[]{2,4,3}));
+        System.out.println(deleteAndEarn(new int[]{2,4}));
         System.out.println(deleteAndEarn(new int[]{2, 2, 3, 3, 3, 4}));
     }
 
