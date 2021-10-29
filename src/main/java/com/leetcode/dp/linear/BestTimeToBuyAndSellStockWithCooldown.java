34ae4f481b5159f27c3b5c2bda56ba9d6e27f5bf
shuangmu <wangchenglinjob@163.com>
2021-10-29 17:59:43 +0800

    从左到右尝试模型

--- src/main/java/com/leetcode/dp/linear/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/com/leetcode/dp/linear/BestTimeToBuyAndSellStockWithCooldown.java
@@ -15,7 +15,7 @@ public class BestTimeToBuyAndSellStockWithCooldown {
      * Design an algorithm to find the maximum profit.
      * You may complete as many transactions as you like
      * (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
-     * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+     * You may not engage in multiple transactions at the same time (ie, you must sell the stock before01 you buy again).
      * After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
      * Example:
      *
@@ -44,10 +44,50 @@ public class BestTimeToBuyAndSellStockWithCooldown {
 
         return max;
     }
+    public static int maxProfit01(int[] prices) {
+        int n = prices.length;
+        Integer[][] dp = new Integer[n][3];
+        return process01(prices, 0, 2, dp);
+    }
 
+    // price 表示前一个状态 0 有 1 表示卖 2 表示冷静期
+    public static int process01(int[] prices, int index, int price, Integer[][] dp) {
+        if (index >= prices.length) {
+            return 0;
+        }
+        if(dp[index][price] != null) {
+            return dp[index][price];
+        }
+        // index 不买不卖
+        int ans = process01(prices, index + 1, price == 1 ? 2 : price, dp);
+
+        if (price == 0) {
+            // index 只能卖
+            ans = Math.max(ans, process01(prices, index + 1, 1, dp) + prices[index]);
+        } else if (price == 2) {
+            // index 只能买
+            ans = Math.max(ans, process01(prices, index + 1, 0, dp) - prices[index]);
+        }
+        dp[index][price] = ans;
+        return ans;
+    }
+
+    public static int maxProfit02(int[] prices) {
+        int n = prices.length;
+        int buy = 0;
+        int sell = 0;
+        int cool = 0;
+        for(int index = n - 1; index >= 0; index--) {
+            int temp = buy;
+            buy = Math.max(buy, sell + prices[index]);
+            sell = cool;
+            cool = Math.max(cool, temp - prices[index]);
+        }
+        return cool;
+    }
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{6,1,6,4,3,0,2}) == 7);
-        System.out.println(maxProfit(new int[]{1,6,0,2}) == 5);
+        System.out.println(maxProfit02(new int[]{6,1,6,4,3,0,2}) == 7);
+        System.out.println(maxProfit02(new int[]{1,6,0,2}) == 5);
     }
 }
