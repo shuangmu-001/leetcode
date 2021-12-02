6d4023ff7a6952b7d0116d0a62e6c43cb77662e7
shuangmu <wangchenglinjob@163.com>
2021-12-02 17:20:46 +0800

    调整代码

--- src/main/java/com/leetcode/hash/FindAllAnagramsInAString.java
+++ src/main/java/com/leetcode/hash/FindAllAnagramsInAString.java
@@ -5,67 +5,69 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * <a href="https://leetcode.com/problems/find-all-anagrams-in-a-string/">Find All Anagrams In A String</a>
+ *
  * @author zms
  * @date 6:51 PM 2020/5/17
- * <a href="https://leetcode.com/problems/find-all-anagrams-in-a-string/">
- * Find All Anagrams In A String</a>
  */
 public class FindAllAnagramsInAString {
     /**
      * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
      * Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
-     *
+     * <p>
      * The order of output does not matter.
-     *
+     * <p>
      * Example 1:
-     *
+     * <p>
      * Input:
      * s: "cbaebabacd" p: "abc"
-     *
+     * <p>
      * Output:
      * [0, 6]
-     *
+     * <p>
      * Explanation:
      * The substring with start index = 0 is "cba", which is an anagram of "abc".
      * The substring with start index = 6 is "bac", which is an anagram of "abc".
      * Example 2:
-     *
+     * <p>
      * Input:
      * s: "abab" p: "ab"
-     *
+     * <p>
      * Output:
      * [0, 1, 2]
-     *
+     * <p>
      * Explanation:
      * The substring with start index = 0 is "ab", which is an anagram of "ab".
      * The substring with start index = 1 is "ba", which is an anagram of "ab".
      * The substring with start index = 2 is "ab", which is an anagram of "ab".
      */
-    public List<Integer> findAnagrams(String s, String p) {
+    public static List<Integer> findAnagrams(String s, String p) {
         int m = s.length();
         int n = p.length();
-        if(m < n) {
+        if (m < n) {
             return new ArrayList<>();
         }
         int[] sfreq = new int[26];
         int[] qfreq = new int[26];
         for (int i = 0; i < n; i++) {
             qfreq[p.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < n; i++) {
             sfreq[s.charAt(i) - 'a']++;
         }
-        List<Integer> res= new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
         for (int i = n; i < m; i++) {
-            if(Arrays.equals(sfreq, qfreq)) {
+            if (Arrays.equals(sfreq, qfreq)) {
                 res.add(i - n);
             }
             sfreq[s.charAt(i) - 'a']++;
-            sfreq[s.charAt(i - n) - 'a']++;
+            sfreq[s.charAt(i - n) - 'a']--;
         }
-        if(Arrays.equals(sfreq, qfreq)) {
+        if (Arrays.equals(sfreq, qfreq)) {
             res.add(m - n);
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findAnagrams("cbaebabacd", "abc"));
     }
 }
--- src/main/java/com/leetcode/hash/PermutationInString.java
+++ src/main/java/com/leetcode/hash/PermutationInString.java
@@ -3,49 +3,47 @@ package com.leetcode.hash;
 import java.util.Arrays;
 
 /**
+ * <a href="https://leetcode.com/problems/permutation-in-string/">Permutation In String</a>
+ *
  * @author zms
  * @date 3:27 PM 2020/5/18
- * <a href="https://leetcode.com/problems/permutation-in-string/">
- * Permutation In String</a>
  * @see FindAllAnagramsInAString
  */
 public class PermutationInString {
     /**
      * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1.
      * In other words, one of the first string's permutations is the substring of the second string.
-     *
+     * <p>
      * Example 1:
-     *
+     * <p>
      * Input: s1 = "ab" s2 = "eidbaooo"
      * Output: True
      * Explanation: s2 contains one permutation of s1 ("ba").
      * Example 2:
-     *
+     * <p>
      * Input:s1= "ab" s2 = "eidboaoo"
      * Output: False
-     *
-     *
+     * <p>
+     * <p>
      * Note:
-     *
+     * <p>
      * The input strings only contain lower case letters.
      * The length of both given strings is in range [1, 10,000].
      */
     public boolean checkInclusion(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
-        if(m > n) {
+        if (m > n) {
             return false;
         }
         int[] s1freq = new int[26];
-        for (int i = 0; i < m; i++) {
-            s1freq[s1.charAt(i) - 'a']++;
-        }
         int[] s2freq = new int[26];
         for (int i = 0; i < m; i++) {
+            s1freq[s1.charAt(i) - 'a']++;
             s2freq[s2.charAt(i) - 'a']++;
         }
         for (int i = m; i < n; i++) {
-            if(Arrays.equals(s1freq, s2freq)) {
+            if (Arrays.equals(s1freq, s2freq)) {
                 return true;
             }
             s2freq[s2.charAt(i) - 'a']++;
