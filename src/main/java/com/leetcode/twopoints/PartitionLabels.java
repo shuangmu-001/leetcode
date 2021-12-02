2c5ec788ac181d8e18b0573374aca923d544b98d
shuangmu <wangchenglinjob@163.com>
2021-12-02 16:58:05 +0800

    字符串小写字母用数组优化

--- src/main/java/com/leetcode/twopoints/PartitionLabels.java
+++ src/main/java/com/leetcode/twopoints/PartitionLabels.java
@@ -10,34 +10,58 @@ import java.util.List;
  */
 public class PartitionLabels {
     /**
-     * A string S of lowercase English letters is given.
+     * A string s of lowercase English letters is given.
      * We want to partition this string into as many parts as possible
      * so that each letter appears in at most one part,
      * and return a list of integers representing the size of these parts.
      *
      * Example 1:
-     * Input: S = "ababcbacadefegdehijhklij"
+     * Input: s = "ababcbacadefegdehijhklij"
      * Output: [9,7,8]
      * Explanation:
      * The partition is "ababcbaca", "defegde", "hijhklij".
      * This is a partition so that each letter appears in at most one part.
-     * A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+     * A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
      *
      *
      * Note:
      *
-     * S will have length in range [1, 500].
-     * S will consist of lowercase English letters ('a' to 'z') only.
+     * s will have length in range [1, 500].
+     * s will consist of lowercase English letters ('a' to 'z') only.
      */
-    public static List<Integer> partitionLabels(String S) {
+    public static List<Integer> partitionLabels01(String s) {
         List<Integer> result = new ArrayList<>();
-        if(S == null) {
+        if(s == null) {
             return result;
         }
         int start = 0;
         int end = 0;
-        for (int i = 0; i < S.length(); i++) {
-            int lastIndex = S.lastIndexOf(S.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            int lastIndex = s.lastIndexOf(s.charAt(i));
+            if(lastIndex > end) {
+                end = lastIndex;
+            }
+            if(i == end) {
+                result.add(i - start + 1);
+                start = i + 1;
+            }
+        }
+        return result;
+    }
+
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        if(s == null) {
+            return result;
+        }
+        int[] ends = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            ends[s.charAt(i) - 'a'] = i;
+        }
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int lastIndex = ends[s.charAt(i) - 'a'];
             if(lastIndex > end) {
                 end = lastIndex;
             }
